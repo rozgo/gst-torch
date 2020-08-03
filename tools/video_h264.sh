60498be39eb9d677b634b73d21cf04c2b3d74d68
rozgo <alex.rozgo@gmail.com>
2020-08-03 14:11:18 -0600

    tweak video tools

--- tools/video_h264.sh
+++ tools/video_h264.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i $1.mkv -pix_fmt yuv420p -c:v h264_nvenc -preset slow -cq 10 -bf 2 -g 150 $2.mp4
+ffmpeg -i $1 -pix_fmt yuv420p -c:v h264_nvenc -preset slow -cq 10 -bf 2 -g 150 $2
